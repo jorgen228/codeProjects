1ebcd00c4d2dc6219994b0ef710fb89752b83844
jorgen228 <j_skjegstad@startmail.com>
2022-12-25 15:18:03 +0100

    started JS work

--- Leieforhold_nettside/script.js
+++ Leieforhold_nettside/script.js
@@ -1,5 +1,41 @@
-let infoPages = document.getElementsByClassName("info-page")
-let infoSection = document.getElementById("info-section")
-console.log(infoPages)
-console.log(infoSection)
+const Page0 = {
+  main: "",
+  second: "",
+  third: "",
+};
+const Page1 = {
+  main: "",
+  second: "",
+  third: "",
+};
+const Page2 = {
+  main: "",
+  second: "",
+  third: "",
+};
+const Page3 = {
+  main: "",
+  second: "",
+  third: "",
+};
+const Page4 = {
+  main: "",
+  second: "",
+  third: "",
+};
+
+let infoObjects = [];
+
+let infoPages = document.getElementsByClassName("info-page");
+let infoSection = document.getElementById("info-section");
+console.log(infoSection);
+
+for (let i = 0; i < infoPages.length; i++) {
+  infoPages[i].id = `info-page-${i}`;
+  infoObjects.push(`Page${i}`);
+}
+
+console.log(infoPages);
+console.log(infoObjects);
+
 

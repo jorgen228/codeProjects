5196746fbdb7becd86ac01f4bd66ad30eb29ac20
jorgen228 <j_skjegstad@startmail.com>
2022-12-28 16:14:31 +0100

    update

--- Leieforhold_nettside/script.js
+++ Leieforhold_nettside/script.js
@@ -1,30 +1,36 @@
-const Page0 = {
-  main: "",
+const page0 = {
+  main: `<p>Lorem <em>ipsum</em> dolor sit amet consectetur adipisicing elit. Non, id explicabo eligendi, ratione molestiae placeat labore deserunt nihil omnis nam, inventore qui fugit sapiente? Iste maiores voluptatem rerum voluptate dolorem.</p>`,
   second: "",
   third: "",
 };
-const Page1 = {
-  main: "",
+const page1 = {
+  main: `<p>Lorem <em>ipsum</em> dolor sit amet consectetur adipisicing elit. Non, id explicabo eligendi, ratione molestiae placeat labore deserunt nihil omnis nam, inventore qui fugit sapiente? Iste maiores voluptatem rerum voluptate dolorem.</p>`,
   second: "",
   third: "",
 };
-const Page2 = {
-  main: "",
+const page2 = {
+  main: `<p>Lorem <em>ipsum</em> dolor sit amet consectetur adipisicing elit. Non, id explicabo eligendi, ratione molestiae placeat labore deserunt nihil omnis nam, inventore qui fugit sapiente? Iste maiores voluptatem rerum voluptate dolorem.</p>`,
   second: "",
   third: "",
 };
-const Page3 = {
-  main: "",
+const page3 = {
+  main: `<p>Lorem <em>ipsum</em> dolor sit amet consectetur adipisicing elit. Non, id explicabo eligendi, ratione molestiae placeat labore deserunt nihil omnis nam, inventore qui fugit sapiente? Iste maiores voluptatem rerum voluptate dolorem.</p>`,
   second: "",
   third: "",
 };
-const Page4 = {
-  main: "",
+const page4 = {
+  main: `<p>Lorem <em>ipsum</em> dolor sit amet consectetur adipisicing elit. Non, id explicabo eligendi, ratione molestiae placeat labore deserunt nihil omnis nam, inventore qui fugit sapiente? Iste maiores voluptatem rerum voluptate dolorem.</p>`,
   second: "",
   third: "",
 };
 
-let infoObjects = [];
+const page5 = {
+  main: `<p>Lorem <span>ipsum</span> dolor sit amet consectetur adipisicing elit. Non, id explicabo eligendi, ratione molestiae placeat labore deserunt nihil omnis nam, inventore qui fugit sapiente? Iste maiores voluptatem rerum voluptate dolorem.</p>`,
+  second: "",
+  third: "",
+};
+
+let infoObjects = [page0, page1, page2, page3, page4, page5];
 
 const infoPages = document.getElementsByClassName("info-page");
 const infoSection = document.getElementById("info-section");
@@ -35,7 +41,7 @@ console.log(infoSection);
 
 for (let i = 0; i < infoPages.length; i++) {
   infoPages[i].id = `info-page-${i}`;
-  infoObjects.push(`Page${i}`);
+  infoObjects.push(`page${i}`);
 }
 
 console.log(infoPages);
@@ -50,20 +56,18 @@ $(".info-page").click(function () {
 });
 
 function updateInfo(index) {
-  let currentObject = infoObjects[index];
-  console.log(currentObject);
+  console.log(infoObjects[index]);
   infoSection.style.height = "85%";
   infoSection.style.backgroundColor = "blue";
   infoText.style.display = "";
   backButton.style.display = "";
-  // infoSection.classList.add("active");
 }
 
 $("#back-button").click(function () {
   infoSection.style.height = 0;
-  infoSection.style.backgroundColor = none;
+  infoSection.style.backgroundColor = "none";
   infoText.style.display = "none";
   backButton.style.display = "none";
   currentObject = "";
   console.log(currentObject);
-})
+});

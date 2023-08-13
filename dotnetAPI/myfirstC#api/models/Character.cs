36e210f50a15d705c3adb8ad3b66b3666fca0222
jorgen228 <j_skjegstad@startmail.com>
2023-08-13 10:54:37 +0200

    Created new api project

---
+++ dotnetAPI/myfirstC#api/models/Character.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myfirstC_api.models
+{
+  public class Character
+  {
+    public int Id { get; set; }
+    public String Name { get; set; } = "Frodo";
+    
+  }
+}
